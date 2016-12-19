a8da9036c4e93b13bbab2e62904c3804cec176d2
Chris Varga <chris.j.varga@gmail.com>
2016-12-18 22:17:36 -0700

    Add layout, main pages, style.css

--- remnant.py
+++ remnant.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "REMNANT FORTRESS"
+    return render_template("index.html")
+
+@app.route("/photo/")
+def photo():
+    return render_template("photo.html")
+
+@app.route("/video/")
+def video():
+    return render_template("video.html")
+
+@app.route("/design/")
+def design():
+    return render_template("design.html")
 
 if __name__ == "__main__":
     app.run()
